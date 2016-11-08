1a5df7a35905bf1df4c22a4fa3a23ecbe885eb24
Kit LAW <kitlaw23@gmail.com>
2016-11-08 10:21:54 +0800

    Delete Radian.java

--- src/mathX/v3/units/Radian.java
+++
@@ -1,8 +0,0 @@
-package mathX.v3.units;
-
-class Radian implements IAngleUnit {
-	@Override
-	public double toRadian(double number) { return number; }
-	@Override
-	public double toUnit(double radian) { return radian; }
-};
