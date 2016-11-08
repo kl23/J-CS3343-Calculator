e3350ba702d2b080c4d2a74d36e73b6a1a43faea
Kit LAW <kitlaw23@gmail.com>
2016-11-08 10:22:01 +0800

    Delete Turn.java

--- src/mathX/v3/units/Turn.java
+++
@@ -1,8 +0,0 @@
-package mathX.v3.units;
-
-class Turn implements IAngleUnit {
-	@Override
-	public double toRadian(double number) { return number * 2.0d * Math.PI; }
-	@Override
-	public double toUnit(double radian) { return radian / Math.PI / 2.0d; }
-};
