bbf9e39bf1ae297e91a7ac754caaed588a578710
Kit LAW <kitlaw23@gmail.com>
2016-11-08 10:14:27 +0800

    Delete Gradian.java

--- src/mathX/v3/units/Gradian.java
+++
@@ -1,8 +0,0 @@
-package mathX.v3.units;
-
-class Gradian implements IAngleUnit {
-	@Override
-	public double toRadian(double number) { return number / 200.0d * Math.PI; }
-	@Override
-	public double toUnit(double radian) { return radian / Math.PI * 200.0d; }
-};
